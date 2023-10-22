243dd98e534db2622f76d72fb6b42d3761ea1876
Barry Flynn <barry.c.flynn@gmail.com>
2023-10-22 11:50:32 +0100

    Add slow print functions to control text speed output

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+
+def slow_print_01(print_statement):
+      for char in print_statement:
+        print(char, end='', flush=True)
+        time.sleep(0.1)
+
+def slow_print_03(print_statement):
+      for char in print_statement:
+        print(char, end='', flush=True)
+        time.sleep(0.3)
+
+
+import time
+
+def slower_print(print_statement):
+      """
+      Function to print a string one character at a time at speed 0.1
+      """
+      for char in print_statement:
+            print(char, end='', flush=True)
+            time.sleep(0.1)
+
+def slow_print(print_statement):
+      """
+      Function to print a string one character at a time at speed 0.01
+      """
+      for char in print_statement:
+            print(char, end='', flush=True)
+            time.sleep(0.01)
+
+def main():
+      """
+      This is the main function that will run the program
+      """
+      slower_print(
+            r""" i am slow 01 this is a test print so see if functions are running
+            """)
+      slow_print(
+            r""" I am slow 03 this is a test print so see if functions are running
+            """)
+
+main()
